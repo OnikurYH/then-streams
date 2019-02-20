0903281ee4f8a68fd2b2488ab63d66d6a2b89626
OnikurYH <onikuryh@gmail.com>
2019-02-21 00:22:09 +0800

    Remove build files

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { Stream } from 'stream';
-export interface ToPromiseOptions {
-    errorEvent?: string;
-    exitEvent?: string;
-}
-export declare function setPromiseClass(promiseClass: PromiseConstructor): void;
-export declare function toPromise<T, S extends Stream>(stream: S, opts?: ToPromiseOptions): Promise<T>;
